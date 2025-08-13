75b40ba17993fad664cb9a1a2587638a195d73fd
Mina-Milaad <minaamilaad4@gmail.com>
2025-08-13 11:12:53 +0300

    bootstrap commit

--- src/modules/bootstrap.js
+++ src/modules/bootstrap.js
@@ -2,6 +2,6 @@ import logRouter from './log//logs.routes.js';
 import collectRouter from './collect/collect.routes.js';
 
 export const bootstrap = (app) => {
-    app.use('/api/logs', collectRouter);
-    app.use('/api/logs', logRouter);
+    app.use('/event/api/logs', collectRouter);
+    app.use('/event/api/logs', logRouter);
 }
