b87c43a350657e76d408c616fba865d51179379d
Mina-Milaad <minaamilaad4@gmail.com>
2025-08-19 16:27:29 +0300

    database

--- database/models/eventLog.model.js
+++ database/models/eventLog.model.js
@@ -4,12 +4,36 @@ import mongoose from "mongoose";
 const schema = new mongoose.Schema({
   method: String,
   endpoint: String,
-  eventType: String,
+  EventType: String,
   userId: mongoose.Schema.Types.Mixed, // ممكن يكون ObjectId أو رقم من الـ .NET
-  username: String,
-  role: { type: String, default: "user" },
+  Username: String,
+  Role: { type: String, default: "user" },
   description: String,
-  ipAddress: String,
+  IpAddress: String,
+  localIPAddress: String,
+  computerName: String,
+  fullUsername: String,      
+  Email: String,
+
+  requestedDates: [{
+    requestDate: Date,
+    hoursRequested: Number,
+    halfDayOption: String,
+    startTime: {
+      hours: Number,
+      minutes: Number,
+      seconds: Number
+    },
+    endTime: {
+      hours: Number,
+      minutes: Number,
+      seconds: Number
+    }
+  }],
+
+  targetDN: String,
+  createdBy: String,
+  fromSecureBrowser: Boolean,
   userAgent: String,
   body: Object,
   query: Object,
