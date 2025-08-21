210f7167ed1d8886687a5cd6bd742a34aa39f266
Mina-Milaad <minaamilaad4@gmail.com>
2025-08-21 16:00:27 +0300

    db local

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,6 +1,11 @@
-import { connect } from "mongoose";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-export const dbConn = connect('mongodb://mongo:27017/eventLog-Project')
-.then(() => {
-    console.log("database Connected Successfully");
-})
+dotenv.config(); // يقرأ متغيرات البيئة من ملف .env
+
+const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/eventLog-Project";
+
+export const dbConn = mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log("Database connected successfully to", MONGO_URI))
+  .catch((err) => console.error("Database connection error:", err));
